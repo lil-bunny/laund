7519d7d7f3e32134e8eba8ab3888d047f0e0dbeb
Tanmoy Dutta <tanmoy.dutta@indusnet.co.in>
2023-07-21 18:10:56 +0530

    dashboard chart added

--- src/components/home/Dashboard.js
+++ src/components/home/Dashboard.js
@@ -1,6 +1,68 @@
 import React from "react";
+import {
+    Chart as ChartJS,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    ArcElement,
+    Tooltip,
+    Legend
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    ArcElement,
+    Tooltip,
+    Legend
+)
+
 
 const Dashboard = () => {
+    const data = {
+        barThickness: 6,
+        barPercentage: 0.5,
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        datasets: [
+            {
+                // label: 'New',
+                data: [3, 6, 9, 5, 8, 2, 6],
+                backgroundColor: '#5C88DA',
+                borderColor: 'black',
+                borderWidth: 1
+            },
+            {
+                // label: 'Delivered',
+                data: [3, 3, 3, 2, 7, 8, 1],
+                backgroundColor: '#1AA301',
+                borderColor: 'black',
+                borderWidth: 1
+            }
+        ]
+    };
+
+    const pieData = {
+        labels: ['One', 'Two', 'Three'],
+        datasets: [
+            {
+                data: [3, 6, 9, 5, 7],
+                backgroundColor: ['#791F06', '#EB5427', '#21D4C9', '#651080', '#E5E62B'],
+                borderWidth: 0,
+            }
+        ]
+    }
+
+    const options = {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+    }
+
     return (
         <>
             <div className="content-header">
@@ -49,47 +111,91 @@ const Dashboard = () => {
                     <span className="break-line"></span>
                     <div className="row">
                         <div className="col-md-6">
-                            <div className="mid-content-box"></div>
+                            <div className="mid-content-box">
+                                <div className="chart-container">
+                                    <div className="chart-heading">
+                                        <h3>Orders & Delivery</h3>
+                                        <span className="blue-box"></span><label>New</label>
+                                        <span className="green-box"></span><label>Delivered</label>
+                                        <span className="red-box"></span><label>Pending</label>
+                                        <div className="select-dropdown table-select">
+                                            <img src="./assets/images/sort-down-small.png" alt="sort-img" />
+                                            <select className="select">
+                                                <option value="">This week</option>
+                                                <option value="1">2nd Week</option>
+                                                <option value="2">3rd week</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <Bar
+                                        data={data}
+                                        options={options}
+                                    />
+                                </div>
+                            </div>
                         </div>
                         <div className="col-md-6">
-                            <div className="mid-content-box"></div>
+                            <div className="mid-content-box">
+                                <div className="pie-chart-container">
+                                    <h3>Service Type Report</h3>
+                                    <div className="select-dropdown table-select">
+                                        <img src="./assets/images/sort-down-small.png" alt="sort-img" />
+                                        <select className="select">
+                                            <option value="">This week</option>
+                                            <option value="1">2nd Week</option>
+                                            <option value="2">3rd week</option>
+                                        </select>
+                                    </div>
+                                    <Pie
+                                        data={pieData}
+                                        options={options}
+                                    />
+                                </div>
+                                <div className="pie-footer">
+                                    <span style={{background: '#EB5427'}} className="square-box"></span><label>Steam Iron</label>
+                                    <span style={{background: '#791F06'}} className="square-box"></span><label>Washing & Pressing</label>
+                                    <span style={{background: '#21D4C9'}} className="square-box"></span><label>Dry Cleaning</label>
+                                    <span style={{background: '#651080'}} className="square-box"></span><label>Other</label>
+                                    <span style={{background: '#E5E62B'}} className="square-box"></span><label>Washing</label>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-3">
-                            <div className="footer-content-box">
-                                <h3>Delivery Boy's</h3>
-                                <div className="footer-content">
-                                    <span>83<p>Active</p></span>
-                                    <span>03<p>Blocked</p></span>
-                                </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="footer-content-box">
+                            <h3>Delivery Boy's</h3>
+                            <div className="footer-content">
+                                <span>83<p>Active</p></span>
+                                <span>03<p>Blocked</p></span>
                             </div>
                         </div>
-                        <div className="col-md-3">
-                            <div className="footer-content-box">
-                                <h3>Helper's</h3>
-                                <div className="footer-content">
-                                    <span>103<p>Active</p></span>
-                                    <span>17<p>Blocked</p></span>
-                                </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="footer-content-box">
+                            <h3>Helper's</h3>
+                            <div className="footer-content">
+                                <span>103<p>Active</p></span>
+                                <span>17<p>Blocked</p></span>
                             </div>
                         </div>
-                        <div className="col-md-3">
-                            <div className="footer-content-box">
-                                <h3>laundrie's</h3>
-                                <div className="footer-content">
-                                    <span>12<p>Active</p></span>
-                                    <span>00<p>Blocked</p></span>
-                                </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="footer-content-box">
+                            <h3>laundrie's</h3>
+                            <div className="footer-content">
+                                <span>12<p>Active</p></span>
+                                <span>00<p>Blocked</p></span>
                             </div>
                         </div>
-                        <div className="col-md-3">
-                            <div className="footer-content-box">
-                                <h3>Customer's</h3>
-                                <div className="footer-content">
-                                    <span>12<p>Active</p></span>
-                                    <span>00<p>Blocked</p></span>
-                                </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="footer-content-box">
+                            <h3>Customer's</h3>
+                            <div className="footer-content">
+                                <span>12<p>Active</p></span>
+                                <span>00<p>Blocked</p></span>
                             </div>
                         </div>
                     </div>
--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -15,7 +15,7 @@ const Location = () =>{
                         <div className="select-dropdown table-select">
                             <img src="./assets/images/sort-down-small.png" alt="sort-img" />
                             <select className="select">
-                                <option value="">select DB</option>
+                                <option value="">Select DB</option>
                                 <option value="1">DB 1</option>
                                 <option value="2">DB 2</option>
                                 <option value="3">DB 3</option>
@@ -24,13 +24,17 @@ const Location = () =>{
                         <div className="select-dropdown table-select">
                             <img src="./assets/images/sort-down-small.png" alt="sort-img" />
                             <select className="select">
-                                <option value="">select helper</option>
+                                <option value="">Select helper</option>
                                 <option value="1">helper 1</option>
                                 <option value="2">helper 2</option>
                                 <option value="3">helper 3</option>
                             </select>
                         </div>
                     </div>
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.27848244319!2d88.42902937502754!3d22.56868547949453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0275ad93c8289b%3A0xc099131033eb5917!2sSDF%20Building%2C%20GP%20Block%2C%20Sector%20V%2C%20Bidhannagar%2C%20Kolkata%2C%20West%20Bengal%20700091!5e0!3m2!1sen!2sin!4v1689939246530!5m2!1sen!2sin" 
+                        width="100%" 
+                        height="550" ></iframe>
                 </div>
             </section>
         </>
